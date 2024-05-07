f44243a2c1ce3920e6fca21e541ff8e90945083b
Nikhil Gangaramani <nikhil.gangaramani@gmail.com>
2024-05-06 20:00:28 -0400

    first commit

---
+++ Joke.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+    function toggleShown() {
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            {/* {isShown && <button onClick={toggleShown}>Hide Punchline</button>}
+            {!isShown && <button onClick={toggleShown}>Show Punchline</button>} */}
+            <button onClick={toggleShown} >{isShown ? "Hide" : "Show"}Punchline </button>
+            <hr />
+        </div>
+    )
+}
